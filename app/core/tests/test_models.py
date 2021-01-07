14cd386cc15182fbe348309b4c32bf3987f439c1
Admin <nguyennguyen8251@gmail.com>
2021-01-07 02:05:30 -0500

    custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,19 +1,41 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-#from app.calc import add, subtract
+# from app.calc import add, subtract
 
-"""inherit TestCase"""
-class ModelTests(TestCase):
+
+class ModelTests(TestCase):  #inherit TestCase
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@gmail.com'
-        password = 'password123'
+        password = 'test123'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))#encrypted
+        self.assertTrue(user.check_password(password))  #encrypted
+
+    def test_new_user_email_normalized(self):
+        """Test email get normalized"""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, password='test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
