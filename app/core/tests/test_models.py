7cba9fbb436b4c6482021e929914ca36a2677d5a
Admin <nguyennguyen8251@gmail.com>
2021-03-11 23:28:00 -0500

    no message

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,3 +63,10 @@ class ModelTests(TestCase):  #inherit TestCase
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            
+        )
